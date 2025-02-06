eb0efc7e956f0a40fe81303a3cf461261caf6ea1
Abbas El <ab-el11@hotmail.com>
2025-02-06 15:33:59 +0100

    added responsive.css

--- js/contactsTemplate.js
+++ js/contactsTemplate.js
@@ -6,8 +6,8 @@ function contactsTemplate(contact) {
     <div class="contact" data-contact-id="${contact.id}" id="contact-${contact.id}" onclick="selectContact(this, '${contact.id}', '${contact.name}', '${contact.email}', '${contact.phone}')">
       <div class="contact-initials" style="background-color: ${bgColor};">${initials}</div>
       <div class="contact-infos">
-        <p style="font-size: 17px">${contact.name}</p>
-        <p style="font-size: 14px; color:#29abe2ff">${contact.email}</p>
+        <p class="contact_name">${contact.name}</p>
+        <p class="contact_email">${contact.email}</p>
       </div>
     </div>
   `;
@@ -19,8 +19,8 @@ function contactDetailsTemplate(id, name, email, phone) {
   return `
     <div >
          <div class="initial_name_content">
-         <div class="contact-initials big-initials" style="background-color: ${bgColor};">${initials}</div>
-            <div class="contact_name"><h1 style="font-size:35px">${name}</h1>
+         <div class=" big-initials" style="background-color: ${bgColor};">${initials}</div>
+            <div class="contact_name"><h1 class="contact-name-h1">${name}</h1>
                 <div class="contact_links"><a href="#" onclick="openEditOverlay('${id}', '${name}', '${email}', '${phone}')"><i class="fa-solid fa-pen"></i>Edit</a>
                     <a href="#" onclick="deleteContact('${id}')" ><i class="fa-solid fa-trash"> </i>Delete</a>
             </div></div>
